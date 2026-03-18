be9a0b2c9003c7c3231d10ad0f12cbe7382f9af7
DF <30237354+heracliteanflux@users.noreply.github.com>
2026-03-18 19:24:34 -0400

    initial commit

---
+++ docs/_static/script.js
@@ -0,0 +1,5 @@
+window.MathJax = {
+  "options": {"processHtmlClass": "tex2jax_process|mathjax_process|math|output_area"},
+  "loader" : {load: ['[tex]/mathtools']},
+  "tex"    : {packages: {'[+]': ['mathtools']}},
+}
